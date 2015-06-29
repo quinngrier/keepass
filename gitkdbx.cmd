51ac4544a9613233f36b85156a878472634e5403
Quinn Grier <quinn.grier@gmail.com>
2015-06-29 13:57:53 -0500

    Use [] in the gitkdbx.cmd copyright notice
    The author of this commit has waived all copyright and
    related or neighboring rights to the extent permitted by
    law as described by the CC0 1.0 Universal Public Domain
    Dedication. You should have received a copy of the full
    dedication along with this commit, typically as a file
    named <CC0-1.0.txt>. If not, it may be available at
    <https://creativecommons.org/publicdomain/zero/1.0/>.

--- gitkdbx.cmd
+++ gitkdbx.cmd
@@ -20,6 +20,6 @@ REM related or neighboring rights to the extent permitted by
 REM law as described by the CC0 1.0 Universal Public Domain
 REM Dedication. You should have received a copy of the full
 REM dedication along with this file, typically as a file
-REM named CC0-1.0.txt. If not, it may be available at
-REM https://creativecommons.org/publicdomain/zero/1.0/.
+REM named [CC0-1.0.txt]. If not, it may be available at
+REM [https://creativecommons.org/publicdomain/zero/1.0/].
 REM
