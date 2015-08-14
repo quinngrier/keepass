7372d54e5982cf37b69e9b356ade7ac91d492ee3
Quinn Grier <quinn.grier@gmail.com>
2015-08-13 22:07:27 -0500

    Rewrite gitkdbx.cmd
    The author of this commit has waived all copyright and
    related or neighboring rights to the extent permitted by
    law as described by the CC0 1.0 Universal Public Domain
    Dedication. You should have received a copy of the full
    dedication along with this commit, typically as a file
    named <CC0-1.0.txt>. If not, it may be available at
    <https://creativecommons.org/publicdomain/zero/1.0/>.

--- gitkdbx.cmd
+++ gitkdbx.cmd
@@ -1,18 +1,5 @@
 @ ECHO OFF
-VERIFY OTHER 2>NUL
-SETLOCAL ENABLEEXTENSIONS
-IF ERRORLEVEL 1 (
-  (ECHO Command extensions could not be enabled.) >&2
-  GOTO END
-)
-SET GIT_WORK_TREE=%0
-SET GIT_WORK_TREE=%GIT_WORK_TREE:"=%
-FOR %%I IN ("%GIT_WORK_TREE%") DO (
-  SET GIT_WORK_TREE=%%~dpI.
-)
-SET GIT_DIR=%GIT_WORK_TREE%\.gitkdbx
-git %*
-:END
+git --git-dir=.gitkdbx --work-tree=. %*
 
 REM
 REM The authors of this file have waived all copyright and
